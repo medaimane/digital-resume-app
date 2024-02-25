37ed9ce5617de33a9d56734040beef9c15673b1e
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2024-02-26 00:10:03 +0100

    Fix ES modules issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: 'dist',
   output: 'export',
   basePath: 'digital-resume-app',
-}
- 
-module.exports = nextConfig
+};
+
+export default nextConfig;
