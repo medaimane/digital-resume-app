65c0a58519c2b06c908a5cd7aac8a8804d307861
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2024-02-25 23:20:03 +0100

    Update configs to enable static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/digital-resume-app",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
