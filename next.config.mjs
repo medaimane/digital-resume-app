bc108a09277dccc65a48f08472b7e3012e0ed1c4
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2024-02-26 00:06:33 +0100

    Set basePath config manually

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,9 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: 'export',
   distDir: 'dist',
+  output: 'export',
+  basePath: 'digital-resume-app',
 }
  
 module.exports = nextConfig
