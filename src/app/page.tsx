14a907dfcd839f32651faa44f4532f31d06eae53
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2024-02-25 22:14:46 +0100

    Update structure of skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,13 +56,29 @@ export default function Page() {
         <h2>Skills & Qualifications</h2>
         <ul id="qualifications--list">
           <li>✔️ 🌟 Top Rated Plus software engineer consultant on Upwork.</li>
+        </ul>
+
+        <p>What I Bring:</p>
+        <ul id="qualifications--list">
           <li>
-            ✔️ 🏆 6+ years specializing in React Native, Expo, and TypeScript.
+            ✔️ 🏆 6+ years specializing in Mobile Apps & FullStack solutions.
           </li>
           <li>
             ✔️ 🚀 Expert in integrating AI, Cloud solutions, and third-party
             platforms.
           </li>
+          <li>
+            ✔️ 🛠 Proficient in setting up CI/CD pipelines for seamless
+            deployment.
+          </li>
+          <li>
+            ✔️ 🤝 Strong sense of team leading, and technical decision-making.
+          </li>
+        </ul>
+
+        <p>Let's Build Something Amazing:</p>
+        <ul id="qualifications--list">
+          <li>✔️ 🏆 Elevate your team as a Tech Lead or Senior Developer.</li>
           <li>
             ✔️ 💡 Creative and critical thinking with excellent communication
             skills.
@@ -70,7 +86,10 @@ export default function Page() {
           <li>
             ✔️ 🛤️ Proficient in MVP launch and product development roadmapping.
           </li>
-          <li>✔️ 🛠 Set up CI/CD pipelines for seamless deployment.</li>
+          <li>
+            ✔️ 🧪 Committed to top-notch quality: Modular, scalable, clean, and
+            well-tested code.
+          </li>
         </ul>
       </section>
 
