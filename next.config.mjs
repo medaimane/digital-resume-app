56ee6fbc5aa607ff647bbb174ad8a72a89e7493e
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2024-02-26 00:02:37 +0100

    Reconfigure actions/configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  output: "export",
-};
-
-export default nextConfig;
+  output: 'export',
+  distDir: 'dist',
+}
+ 
+module.exports = nextConfig
