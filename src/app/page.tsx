7d8b6c22e9e6a1dedc3da43a3380e8d98f0cf461
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2024-02-25 20:07:34 +0100

    Add initial resume page with hero section completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,140 @@
-import Image from "next/image";
+import Image from 'next/image';
 
-export default function Home() {
+export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <div id="container--main">
+      <section id="wrapper--hero" className="section--page">
+        <Image
+          id="profile-pic"
+          src="/avatars/me.jpeg"
+          alt="Profile Avatar"
+          width={150}
+          height={150}
+        />
+
+        <div>
+          <h1 id="user-name">Mohamed Aimane Skhairi</h1>
+          <p id="bio">
+            Senior Software engineer, Mobile Apps and Full Stack Development,
+            Freelancer Consultant at{' '}
+            <a
+              href="https://www.upwork.com/freelancers/medaimane"
+              target="_blank"
+            >
+              Upwork
+            </a>
+            , React Native Expert.
+          </p>
+          <p id="email">👉 medaimane.skhairi@gmail.com</p>
+        </div>
+      </section>
+
+      <section className="section--page">
+        <div id="socials--list">
+          <a href="https://instagram.com/medaimane" target="_blank">
+            📸 Instagram
+          </a>
+          <a href="https://twitter.com/med_aimane" target="_blank">
+            ☕️💡 X (Twitter)
+          </a>
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://www.linkedin.com/in/med_aimane_skahiri/"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            🤝 LinkedIn
+          </a>
+          <a href="https://github.com/medaimane" target="_blank">
+            👨‍💻 Github
+          </a>
+          <a href="./assets/pdf/resume.pdf" target="_blank">
+            📜 Download Resume
           </a>
         </div>
-      </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className="section--page">
+        <h2>Skills & Qualifications</h2>
+        <ul id="qualifications--list">
+          <li>✔️ 🌟 Top Rated Plus software engineer consultant on Upwork.</li>
+          <li>
+            ✔️ 🏆 6+ years specializing in React Native, Expo, and TypeScript.
+          </li>
+          <li>
+            ✔️ 🚀 Expert in integrating AI, Cloud solutions, and third-party
+            platforms.
+          </li>
+          <li>
+            ✔️ 💡 Creative and critical thinking with excellent communication
+            skills.
+          </li>
+          <li>
+            ✔️ 🛤️ Proficient in MVP launch and product development roadmapping.
+          </li>
+          <li>✔️ 🛠 Set up CI/CD pipelines for seamless deployment.</li>
+        </ul>
+      </section>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+      <section className="section--page">
+        <h2>Tech stack</h2>
+
+        <div id="wrapper--techstack__items">
+          <div className="card--techstack">
+            <span>
+              TypeScript, JavaScript, Python, Java/Kotline, Objective-C/Swift
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          </div>
+          <div className="card--techstack">
+            <span>React Native, Expo, iOS/Android Apps</span>
+          </div>
+          <div className="card--techstack">
+            <span>React, NextJS, Strapi, NodeJS</span>
+          </div>
+          <div className="card--techstack">
+            <span>AWS, OpenAI, Firebase, Google Cloud</span>
+          </div>
+          <div className="card--techstack">
+            <span>SQLite, Postgres, MongoDB</span>
+          </div>
+          <div className="card--techstack">
+            <span>GitHub, ExpoEAS, TestFlight, App Center, Vercel</span>
+          </div>
+        </div>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <section id="work-history-wrapper" className="section--page">
+        <h2>Work History</h2>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        <div className="line-break"></div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className="card--work-history">
+          <strong>
+            🚧 Mobile Apps & FullStack Engineer Consultant | UpWork
+          </strong>
+          <p>12/2021 - Present</p>
+          <p>Description...</p>
+          <ul>
+            <li>Point one</li>
+            <li>Point two</li>
+            <li>Point three</li>
+          </ul>
+        </div>
+      </section>
+
+      <section className="section--page">
+        <h2>Projects & Accomplishments</h2>
+
+        <div className="card--project">
+          <a href="project1.html">
+            <span>🏆 </span>Project One
+          </a>
+        </div>
+
+        <div className="card--project">
+          <a href="project1.html">
+            <span>🏆 </span>Project Two
+          </a>
+        </div>
+      </section>
+    </div>
   );
 }
