6725baccbfa7caeff18486cc886951958871d3a7
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2024-02-26 02:31:08 +0100

    Configure next based on node env (prod/dev)

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const prodConfig = {
   distDir: 'dist',
-  basePath: '/digital-resume-app',
+  output: 'export',
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
+  basePath: '/digital-resume-app',
 };
+
+const devConfig = {};
+
+const nextConfig = isProd ? prodConfig : devConfig;
+
+console.log('isProd', isProd);
 
 export default nextConfig;
